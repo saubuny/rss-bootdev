2d968d807f38170e5a34d1b9be76bfc78ff95900
saubuny <saubuny@gmail.com>
2024-08-27 10:43:46 -0400

    create feed

--- main.go
+++ main.go
@@ -46,6 +46,32 @@ func respondWithJSON(w http.ResponseWriter, code int, payload any) {
 	w.Write(dat)
 }
 
+type authedHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func (cfg *apiConfig) middlewareAuth(handler authedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		headerAuth := r.Header.Get("Authorization")
+		if headerAuth == "" {
+			respondWithError(w, 401, "Authorization header missing")
+			return
+		}
+
+		apiKey := strings.TrimPrefix(headerAuth, "ApiKey ")
+		if apiKey == headerAuth {
+			respondWithError(w, 401, "Malformed Token")
+			return
+		}
+
+		user, err := cfg.DB.GetUserByApiKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithError(w, 500, "Error getting user by ApiKey: "+err.Error())
+			return
+		}
+
+		handler(w, r, user)
+	}
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	type res struct {
 		Status string `json:"status"`
@@ -79,16 +105,28 @@ func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: name.Name})
+	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name.Name,
+	})
 
 	if err != nil {
 		respondWithError(w, 500, "Error Creating User: "+err.Error())
 		return
 	}
 
-	respondWithJSON(w, 200, res{ID: user.ID, CreatedAt: user.CreatedAt, UpdatedAt: user.UpdatedAt, Name: name.Name, ApiKey: user.ApiKey})
+	respondWithJSON(w, 200, res{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Name:      name.Name,
+		ApiKey:    user.ApiKey,
+	})
 }
 
+// TODO: Use the Auth Middleware for this !!
 func (cfg *apiConfig) getUserByApiKeyHandler(w http.ResponseWriter, r *http.Request) {
 	headerAuth := r.Header.Get("Authorization")
 	if headerAuth == "" {
@@ -116,7 +154,45 @@ func (cfg *apiConfig) getUserByApiKeyHandler(w http.ResponseWriter, r *http.Requ
 		ApiKey    string    `json:"api_key"`
 	}
 
-	respondWithJSON(w, 200, res{ID: user.ID, CreatedAt: user.CreatedAt, UpdatedAt: user.UpdatedAt, Name: user.Name, ApiKey: apiKey})
+	respondWithJSON(w, 200, res{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Name:      user.Name,
+		ApiKey:    apiKey,
+	})
+}
+
+func (cfg *apiConfig) createFeedHandler(w http.ResponseWriter, r *http.Request, user database.User) {
+	type body struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+
+	var b body
+	req, _ := io.ReadAll(r.Body)
+	err := json.Unmarshal(req, &b)
+
+	if err != nil {
+		respondWithError(w, 500, err.Error())
+		return
+	}
+
+	feed, err := cfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      b.Name,
+		Url:       b.Url,
+		UserID:    user.ID,
+	})
+
+	if err != nil {
+		respondWithError(w, 500, "Error Creating Feed: "+err.Error())
+		return
+	}
+
+	respondWithJSON(w, 200, feed)
 }
 
 func main() {
@@ -133,6 +209,7 @@ func main() {
 	serveMux.HandleFunc("GET /v1/err", errorHandler)
 	serveMux.HandleFunc("POST /v1/users", cfg.createUserHandler)
 	serveMux.HandleFunc("GET /v1/users", cfg.getUserByApiKeyHandler)
+	serveMux.HandleFunc("POST /v1/feeds", cfg.middlewareAuth(cfg.createFeedHandler))
 
 	server := http.Server{Handler: serveMux, Addr: "localhost:" + port}
 	fmt.Println("[Info] Starting server on port", 8080)
---
+++ sql/queries/feeds.sql
@@ -0,0 +1,5 @@
+-- name: CreateFeed :one
+INSERT INTO feeds (id, user_id, created_at, updated_at, name, url)
+VALUES ($1, $2, $3, $4, $5, $6)
+RETURNING *;
+
